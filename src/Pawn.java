714735f6ce9f3f15b4b3abe15874fc2783c5c3fd
Andrey-Kovalchuk <kovalc678@gmail.com>
2025-01-30 18:01:58 +0200

    логика для фигуры Король

--- src/Pawn.java
+++ src/Pawn.java
@@ -1,2 +1,23 @@
 public class Pawn {
+
+    public Pawn () {
+
+    }
+    private static String [][] move (String [][] desk, int bX, int bY, int aX, int aY) {
+        if (aX<2   || aX > 11  || aY< 2 || aY > 11 )  {
+            return desk;
+        }
+        if (bX == bX && aY == aY)
+            return desk;
+
+        if (bX == aX) {
+
+        }
+       return desk;
+    }
+
+
+
+
+
 }
