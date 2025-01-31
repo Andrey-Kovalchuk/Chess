a9960216b24dd6913e9f0b175c0993cbd112f2db
Андрей <Kovalc678@gmail.com>
2025-01-31 14:20:49 +0200

    все фигуры
    надо пешка

--- src/King.java
+++ src/King.java
@@ -1,2 +1,36 @@
 public class King {
+
+    public void king() {
+
+    }
+
+
+    private static void gameOver() {
+        System. exit(0);
+    }
+    private static String [][] move (String [][] desk, int bX, int bY, int aX, int aY) {
+
+    if (desk[aX][aY].equals("K")){
+        return desk;
+    }
+    if (aX<2   || aX > 11  || aY< 2 || aY > 11 )  {
+        return desk;
+    }
+    if (bX == aX && bY == aY)
+        return desk;
+
+    if ((  1 >=  bX-aX) || (bX-aX >= - 1) && (bY-aY <= 1 )|| ( bY-aY >=-1) ){
+        desk[aX][aY] = "K";
+        desk[bX][bY] = " ";
+        return desk;
+    }
+
+    return desk ;
+    }
+
+
+
+
+
+
 }
--- src/Knight.java
+++ src/Knight.java
@@ -1,2 +1,66 @@
 public class Knight {
+     static int bX;
+     static int aX;
+     static int bY;
+     static int aY;
+     static String[][] desk;
+    private String[][] Knight(String[][] desk, int bX, int bY, int aX, int aY) {
+         this.bX = bX;
+         this.bY = bY;
+         this.aX = aX;
+         this.aY = aY;
+         this.desk = desk;
+        if (bX == aX && bY == aY)
+            return desk;
+        if (aX<2   || aX > 11  || aY< 2 || aY > 11 )  {
+            return desk;
+        }
+        if (bX-aX == 2) {
+            if (bY-aY == 1) {
+                loadChenges();
+            }
+            else if (bY-aY == -1) {
+                loadChenges();
+            } else {
+                return desk;
+            }
+        }
+        else if (bX-aX == -2) {
+            if (bY-aY == 1) {
+                loadChenges();
+            }
+            else if (bY-aY == -1) {
+                loadChenges();
+            } else {
+                return desk;
+            }
+        } if (bY-aY == 2) {
+            if (bX-aX == 1) {
+                loadChenges();
+            }
+            else if (bX-aX == -1) {
+                loadChenges();
+            } else {
+                return desk;
+            }
+        }
+        else if (bY-aY == -2) {
+            if (bX-aX == 1) {
+                loadChenges();
+            }
+            else if (bX-aX == -1) {
+                loadChenges();
+            } else {
+                return desk;
+            }
+        }
+        return desk;
+    }
+
+private static void loadChenges() {
+    desk [bX][bY] = " ";
+    desk [aX][aY] = "H";
+}
+
+
 }
